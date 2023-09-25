800a3a1631b728f8eabd1ecbd239e987e8c612a0
Dorovich <silverdovi25@gmail.com>
2023-09-25 18:13:59 +0200

    sesion 1 terminada

--- user.c
+++ user.c
@@ -4,10 +4,8 @@ char buff[24];
 
 int pid;
 
-int add(int a, int b)
-{
-	return a + b;
-}
+int add(int a, int b) { return a + b; }
+int addAsm(int a, int b);
 
 int __attribute__ ((__section__(".text.main")))
   main(void)
@@ -15,7 +13,8 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-  int r = add(0x69, 0b010101001);
+  /* int r = add(0x69, 0b010101001); */
+  int r = addAsm(32, 37);
     
-  while(1) { }
+  while(1);
 }
