0dbe76315e64459a54a4203565a9b2702e49849c
Dorovich <silverdovi25@gmail.com>
2023-10-02 10:01:29 +0200

    page fault raro

--- user.c
+++ user.c
@@ -12,15 +12,19 @@ int __attribute__ ((__section__(".text.main")))
 {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+    //char msg[256] = "Hola xd.";
+    //write(1, msg, strlen(msg));
 
-  char msg[256] = "Hola xd.";
-  write(1, msg, strlen(msg));
 
-  int t;
+  //int t = 0;
   while(1) {
-    for(int i=0; i<1000; ++i);
-    t = gettime();
-    itoa(t, msg);
-    write(1, msg, strlen(msg));
+    //t = gettime();
+    for (int i = 3000; i < 100000; i = i + 1) {
+      char msg[256];
+      itoa(i, msg);
+      write(1, msg, strlen(msg));
+      char* c = " ";
+      write(1, c, strlen(c));
+    }
   }
 }
