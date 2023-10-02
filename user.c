bd812ae6f7b549e313069521dbbcff0c0f394908
Dorovich <silverdovi25@gmail.com>
2023-10-02 09:48:12 +0200

    sysenter, se va a la shit

--- user.c
+++ user.c
@@ -13,12 +13,14 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-  /* int r = add(0x69, 0b010101001); */
-  /* int r = addAsm(32, 37); */
-
   char msg[256] = "Hola xd.";
-  write(-1, msg, strlen(msg));
-  perror();
+  write(1, msg, strlen(msg));
 
-  while(1);
+  int t;
+  while(1) {
+    for(int i=0; i<1000; ++i);
+    t = gettime();
+    itoa(t, msg);
+    write(1, msg, strlen(msg));
+  }
 }
