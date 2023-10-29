e53b789f8f12f4c13894637e9244f856e42db8d0
Dorovich <silverdovi25@gmail.com>
2023-10-29 16:04:01 +0100

    exit

--- user.c
+++ user.c
@@ -15,37 +15,15 @@ int __attribute__ ((__section__(".text.main")))
   /* t = gettime(); */
   
   char buf[256];
-  
-  /* int n = 12345; */
-  /* itoa(n, buf); */
-  /* itoa(n, buf); */
-  /* int written = write(1, buf, strlen(buf)); */
-  /* itoa(written, buf); */
-  /* write(1, buf, strlen(buf)); */
-    
-  /* int n2 = 67890; */
-  /* itoa(n2, buf); */
-  /* written = write(1, buf, strlen(buf)); */
-  /* itoa(written, buf); */
-  /* write(1, buf, strlen(buf)); */
-  /* itoa(n2, buf); */
-  /* written = write(1, buf, strlen(buf)); */
-  /* itoa(written, buf); */
-  /* write(1, buf, strlen(buf)); */
 
-  write(-1, buf, strlen(buf));
+  /* write(-1, buf, strlen(buf)); */
   /* perror(); */
-  
-  /* buf[0] = 'A'; */
-  /* buf[1] = ' '; */
-  /* buf[2] = '\0'; */
 
   int pid = fork();  
   itoa(pid, buf);
     
   while(1) {
       for (unsigned int i=0; i<500000; ++i);
-      /* write(1, buf, strlen(buf)); */
       write(1, buf, strlen(buf));
   }
 }
