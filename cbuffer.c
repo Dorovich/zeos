23c55c4ce3dd8da9081a5ef82043d46ea0a47056
Morsoth <morsoth119@gmail.com>
2023-11-13 09:44:12 +0100

    cbuffer

---
+++ cbuffer.c
@@ -0,0 +1,18 @@
+#include <cbuffer.h>
+
+char cbuffer_pop(struct cbuffer *b) {
+    
+}
+
+void cbuffer_push(struct cbuffer *b, char data) {
+
+}
+
+int cbuffer_empty(struct cbuffer *b) {
+
+}
+
+int cbuffer_full(struct cbuffer *b) {
+
+}
+
---
+++ include/cbuffer.h
@@ -0,0 +1,19 @@
+#ifndef CBUFFER_H
+#define CBUFFER_H
+
+#define CBUFFER_SIZE 10
+
+struct cbuffer {
+		char buffer[CBUFFER_SIZE];
+		int head, tail;
+}
+
+char cbuffer_pop (struct cbuffer *b);
+
+void cbuffer_push (struct cbuffer *b, char data);
+
+int cbuffer_empty (struct cbuffer *b);
+
+int cbuffer_full (struct cbuffer *b);
+
+#endif /* CBUFFER_H */
