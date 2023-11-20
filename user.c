ff753ff2ec1e1f8ee03c87a5829dfbc5bc6b1e60
morsoth119 <morsoth119@gmail.com>
2023-11-20 09:41:25 +0100

    testing

--- user.c
+++ user.c
@@ -10,64 +10,8 @@ int addAsm(int a, int b);
 int __attribute__ ((__section__(".text.main")))
 main(void)
 {
-    char buf[1024] = "Hola que tal, este es un mensaje superlargo que he hecho solo para que exceda el tamanyo del buffer de escritura del sistema operativo y comprovar que funciona como deberia. No te preocupes, si todo funciona bien no deberia pasar nada raro, pero porsiacaso voy a comprobarlo igualmente... Lo que deberia hacer el sistema operativo es copiar y ecribir este mensaje por fragmentos, ya que hay tantos caracteres que excede el valor de la variable WRITE_MAX (256) que tiene definido. Este mensaje tiene 520 caracteres jaja.\n";
-
-    int written = write(1, buf, strlen(buf));
-    itoa(written, buf);
-    write(1, buf, strlen(buf));
-    
-    /* perror(); */
-    
-    /* int t = gettime(); */
-    /* for(int i = 0; i < 10000000; ++i); */
-    /* t = gettime(); */
-
-    if (fork() == 0) {
-        char b[24] = "Hola soy el hijo :)\n\0";
-        write(1, b, strlen(b));
-        exit();
-    } else {
-        char b[24] = "Hola soy el padre :)\n\0";
-        write(1, b, strlen(b));
-    }
-    
-    while(1) {
-        /* struct stats s; */
-        
-        /* for (unsigned int i=0; i<1000000; ++i); */
-        /* get_stats(getpid(), &s); */
-
-        /* buf[0] = '['; */
-        /* buf[1] = '\0'; */
-        /* write(1, buf, strlen(buf)); */
-        
-        /* itoa(s.user_ticks, buf); */
-        /* write(1, buf, strlen(buf)); */
-        /* buf[0] = ','; */
-        /* buf[1] = ' '; */
-        /* buf[2] = '\0'; */
-        /* write(1, buf, strlen(buf)); */
-        
-        /* itoa(s.system_ticks, buf); */
-        /* write(1, buf, strlen(buf)); */
-        /* buf[0] = ','; */
-        /* buf[1] = ' '; */
-        /* buf[2] = '\0'; */
-        /* write(1, buf, strlen(buf)); */
-        
-        /* itoa(s.ready_ticks, buf); */
-        /* write(1, buf, strlen(buf)); */
-        /* buf[0] = ','; */
-        /* buf[1] = ' '; */
-        /* buf[2] = '\0'; */
-        /* write(1, buf, strlen(buf)); */
-        
-        /* itoa(s.elapsed_total_ticks, buf); */
-        /* write(1, buf, strlen(buf)); */
-        
-        /* buf[0] = ']'; */
-        /* buf[1] = ' '; */
-        /* buf[2] = '\0'; */
-        /* write(1, buf, strlen(buf)); */
-    }
+    char c;
+    int err = waitKey(&c, 5);
+    if (!err) write(1, &c, 1);
+    while(1);
 }
